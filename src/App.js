f8a46efb5a0e455e578de1f493a8cb3000ddc8d2
GodZkotm54 <41457922+rlennert95@users.noreply.github.com>
2019-02-15 18:17:27 -0500

    logo comp made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav/nav.js'
+import Logo from './components/logo/logo.js'
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
 
+      <Logo/>
       <Nav/>
    
 
---
+++ src/components/logo/logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AnthemLogo from '../../assets/Anthem_Logo.png'
+
+function Logo() {
+  return (
+   <img src={AnthemLogo}/>
+  );
+}
+
+export default Logo;
