be1685ea9e749447145efa7bac3f09b7bfc0bb6c
GodZkotm54 <41457922+rlennert95@users.noreply.github.com>
2019-02-15 18:02:49 -0500

    deleted stock React css

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './components/Nav/nav.js'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      <Nav/>
+   
+
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Nav/nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Nav() {
+  return (
+   <div> NAVBAR GOES HERE </div>
+  );
+}
+
+export default Nav;
